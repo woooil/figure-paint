1219db4848f5acb9024ad1def81a00822f870993
wooil <wooilkim9@gmail.com>
2023-03-02 23:09:09 +0900

    feat: add hints for label of point

--- src/paper/Label/CreateLabelByClick.js
+++ src/paper/Label/CreateLabelByClick.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 
-import { create, setDep } from "../../Figure/figureSlice";
+import { create, update, remove, setDep } from "../../Figure/figureSlice";
 import { TYPE } from "../../Figure/Figure";
 import Label from "../../Figure/Label";
 import clickJudge from "../clickJudge";
@@ -10,17 +10,37 @@ function CreateLabelByClick() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const handleMouseClick = (event) => {
+    var hint = undefined;
+    var activeId = false;
+    const handleMouseMove = (event) => {
       const element = clickJudge(event, TYPE.Point);
-      if (element !== undefined) {
-        const figure = Label.byPointName(element, 0, -10);
-        dispatch(create(figure));
-        dispatch(setDep({ determinant: element, dependant: figure.id }));
+      if (element !== undefined && element !== activeId) {
+        hint = Label.byPointName(element, 0, -10);
+        dispatch(create(hint));
+        dispatch(setDep({ determinant: element, dependant: hint.id }));
+        dispatch(update({ id: hint.id, with: { isHint: true } }));
+        dispatch(update({ id: element, with: { isHint: true } }));
+        activeId = element;
+      } else if (element === undefined && hint) {
+        dispatch(update({ id: activeId, with: { isHint: false } }));
+        dispatch(remove(hint.id));
+        hint = undefined;
+        activeId = false;
+      }
+    };
+    const handleMouseClick = (event) => {
+      if (hint) {
+        dispatch(update({ id: activeId, with: { isHint: false } }));
+        dispatch(update({ id: hint.id, with: { isHint: false } }));
+        hint = undefined;
+        activeId = false;
       }
     };
     window.addEventListener("click", handleMouseClick);
+    window.addEventListener("mousemove", handleMouseMove);
     return () => {
       window.removeEventListener("click", handleMouseClick);
+      window.removeEventListener("mousemove", handleMouseMove);
     };
   });
 
